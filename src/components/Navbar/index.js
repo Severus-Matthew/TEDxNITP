7986f6385c090c6e5e54e1461b493889592abe4b
vikhyat singh <vikhyatsingh628@gmail.com>
2021-01-14 01:57:55 +0530

    Update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import React from "react"
 //import tedx from '../../images/TedX.png'
 import style from './style.module.scss'
-import tedlogo from '../../images/lgw.png'
+import tedlogo from '../../images/lgb.png'
 //import styled from "@emotion/styled"
 //import { css } from "@emotion/core"
 
@@ -16,7 +16,7 @@ const Navbar =()=>
     <div className={style.container}>
         <img src={tedlogo} className={style.tedlogo} alt='tedxlogo'/>
         <div className={style.pages}>
-       
+
             <Link className={style.links} activeClassName={style.activelinks}  to="/">Home </Link>
             <Link className={style.links} activeClassName={style.activelinks} to="/speakers">Speakers</Link>
             <Link className={style.links} activeClassName={style.activelinks} to="/about">About</Link>
@@ -27,7 +27,7 @@ const Navbar =()=>
 
     </div>
     </div>
-    
+
 )
 
 
