b0d726d864e76ffd3e52f5d95d972c1afb760aea
Severus-Matthew <drprofmjha@gmail.com>
2021-01-13 17:44:57 +0530

    editing footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import style from './style.module.scss'
 import Container from '../Container/index'
 import tedlogo from '../../images/1.png'
-import { FaInstagram, FaEnvelopeOpen, FaPhoneAlt } from 'react-icons/fa';
+import { FaInstagram, FaEnvelopeOpen, FaPhoneAlt, FaLinkedin } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
 
@@ -15,9 +15,10 @@ const Footer =()=>
             <div className={style.container}>
                     <div className={style.content_holder}>
                         <img src={tedlogo} style={{width:'200px', height:'70px'}}/>
-                        <p className={style.insta2}><IconContext.Provider value={{ size: '30px' }}> <FaInstagram/></IconContext.Provider><a className={style.insta}href=""><br /><span className={style.red} ></span></a>  </p>
-                        <p className={style.insta}><IconContext.Provider value={{ size: '30px' }}> <FaEnvelopeOpen /></IconContext.Provider> <span className={style.red}> <br /></span>  </p>
-                        <p className={style.insta}><IconContext.Provider value={{ size: '30px' }}><FaPhoneAlt /></IconContext.Provider> <span className={style.red}> <br /> </span>  </p>
+                        <p className={style.insta2}><IconContext.Provider className={style.icon} value={{ size: '37px' }}> <FaInstagram/></IconContext.Provider><a className={style.insta}href=""><br /><span className={style.red} ></span></a>  </p>
+                        <p className={style.insta}><IconContext.Provider value={{ size: '37px' }}> <FaEnvelopeOpen /></IconContext.Provider> <span className={style.red}> <br /></span>  </p>
+                        <p className={style.insta}><IconContext.Provider value={{ size: '37px' }}><FaLinkedin /></IconContext.Provider> <span className={style.red}> <br /> </span>  </p>
+                        <p className={style.insta}><IconContext.Provider value={{ size: '37px'}}><FaPhoneAlt /></IconContext.Provider> <span className={style.red}> <br /> </span>  </p>
                         <p className={style.news}> SignUp For Newsletter</p>
                 </div>
                 <p className={style.license}>"This Independent TEDx event is operated under license from TED." </p>
