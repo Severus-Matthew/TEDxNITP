28d2fd01a9622b1ab5b82e1d59ef1f22a00c431c
Severus-Matthew <drprofmjha@gmail.com>
2021-03-13 03:22:14 +0530

    partner1

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -3,6 +3,9 @@ import style from './sass/sponsor.module.scss'
 import Layout from '../components/layout'
 import Container from '../components/Container'
 import { StaticQuery, graphql } from "gatsby";
+import rft from '../pages/Riyft.png'
+import ww from '../pages/ww.png'
+import vlcc from '../pages/vlcc.png'
 //import Img from 'gatsby-image';
 
 
@@ -40,12 +43,40 @@ const Sponsors=()=>
         <div className={style.section}>
             <Container>
             <p className={style.title}> Our <span className={style.red}>Sponsors</span></p>
-            <div className={style.container}>
-
-          <h1 className={style.sponsor}>Coming Soon...</h1>
-
-            </div>
-           <p className={style.sponsor}>Want to Sponsor Us ? Mail us on <span className={style.red}>tedxnitpatna@gmail.com </span>to know more.</p>
+                <div className={style.container}>
+                    <div className={style.row}>
+                        <div classname={style.column1}>
+                    <div className={style.card}>
+                        <img className={style.card} src={rft} />
+                    <div className={style.container2}>
+                                    <h4 className={style.comname}><b>RIYFT</b></h4>
+                                    <p className={style.prt}>STRATEGIC PARTNER</p>
+                        </div>
+                            </div>
+                        </div>
+                        <div classname={style.column1}>
+                            <div className={style.card}>
+                                <img className={style.card} src={ww} />
+                                <div className={style.container2}>
+                                    <h4 className={style.comname}>Wealthy Waste</h4>
+                                    <p className={style.prt}>GIFT PARTNER</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div classname={style.column1}>
+                            <div className={style.card}>
+                                <img className={style.card} src={vlcc} />
+                                <div className={style.container2}>
+                                    <h4 className={style.comname}>VLCC</h4>
+                                    <p className={style.prt}>GROOMING PARTNER</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                </div>
+              
+                <p className={style.sponsor} style={{marginTop:'170px'}}>Want to Sponsor Us ? Mail us on <span className={style.red}>tedxnitpatna@gmail.com </span>to know more.</p>
 
             </Container>
 
